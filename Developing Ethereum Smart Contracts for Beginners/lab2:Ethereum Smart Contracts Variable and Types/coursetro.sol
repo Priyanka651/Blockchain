4cd3dc6df0a77bd800161890f514c70345e79518
Priyanka651 <Pariprajapati2109@gmail.com>
2024-12-02 17:55:59 -0800

    coursetro.sol

--- Developing Ethereum Smart Contracts for Beginners/lab2:Ethereum Smart Contracts Variable and Types/coursetro.sol
+++ Developing Ethereum Smart Contracts for Beginners/lab2:Ethereum Smart Contracts Variable and Types/coursetro.sol
@@ -1,1 +1,25 @@
 
+pragma solidity ^0.4.18;
+
+contract Coursetro {
+    string fName;
+    uint age;
+
+    // Constructor to initialize default values
+    constructor() public {
+        fName = "gary";
+        age = 34;
+    }
+
+    // Function to set instructor details
+    function setInstructor(string _fName, uint _age) public {
+        fName = _fName;
+        age = _age;
+    }
+
+    // Function to get instructor details
+    function getInstructor() public constant returns (string, uint) {
+        return (fName, age);
+    }
+}
+
