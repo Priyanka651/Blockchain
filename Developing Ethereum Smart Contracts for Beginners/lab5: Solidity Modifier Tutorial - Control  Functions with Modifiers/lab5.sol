2e262b1684950ed8fd8250d077eb8a8d18e606b6
Priyanka651 <Pariprajapati2109@gmail.com>
2024-12-02 18:17:16 -0800

    lab5.sol

--- Developing Ethereum Smart Contracts for Beginners/lab5: Solidity Modifier Tutorial - Control  Functions with Modifiers/lab5.sol
+++ Developing Ethereum Smart Contracts for Beginners/lab5: Solidity Modifier Tutorial - Control  Functions with Modifiers/lab5.sol
@@ -1,1 +1,38 @@
+pragma solidity ^0.4.18;
+
+contract Coursetro {
+    string fName;
+    uint age;
+    address owner;
+
+    // Constructor to initialize default values
+    constructor()public {
+        fName = "gary";
+        age = 34;
+        owner = msg.sender;
+    }
+    event Instructor(
+        string name,
+        uint age
+    );
+    modifier onlyOwner() {
+        require(msg.sender ==owner);
+        _;
+    }
+
+    // Function to set instructor details
+    function setInstructor(string memory _fName, uint _age) public 
+    onlyOwner
+    {
+        fName = _fName;
+        age = _age;
+        emit Instructor(_fName, _age);
+
+    }
+
+    // Function to get instructor details
+    function getInstructor() public constant returns (string memory, uint) {
+        return (fName, age);
+    }
+}
 
