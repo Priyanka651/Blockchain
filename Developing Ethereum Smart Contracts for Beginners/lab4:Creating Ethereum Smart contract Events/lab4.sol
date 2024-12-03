d720f0a2816c91e9419942f8979d9607b3f119d9
Priyanka651 <Pariprajapati2109@gmail.com>
2024-12-02 18:12:54 -0800

    lab4.sol

--- Developing Ethereum Smart Contracts for Beginners/lab4:Creating Ethereum Smart contract Events/lab4.sol
+++ Developing Ethereum Smart Contracts for Beginners/lab4:Creating Ethereum Smart contract Events/lab4.sol
@@ -1,1 +1,30 @@
+pragma solidity ^0.4.18;
+
+contract Coursetro {
+    string fName;
+    uint age;
+
+    // Constructor to initialize default values
+    constructor()public {
+        fName = "gary";
+        age = 34;
+    }
+    event Instructor(
+        string name,
+        uint age
+    );
+
+    // Function to set instructor details
+    function setInstructor(string memory _fName, uint _age) public {
+        fName = _fName;
+        age = _age;
+        emit Instructor(_fName, _age);
+
+    }
+
+    // Function to get instructor details
+    function getInstructor() public constant returns (string memory, uint) {
+        return (fName, age);
+    }
+}
 
